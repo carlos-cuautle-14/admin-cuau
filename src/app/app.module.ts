579db6c1b175fce624a53a2e0f34309a246f51f9
everis <everis@MEX-F8SCPH2.usersad.everis.int>
2018-06-07 19:12:58 -0500

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { APP_ROUTES } from './app.routes';
 
 //Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { IncrementadorComponent } from './components/incrementador/incrementador.component';
+
+
 
 
 
@@ -24,7 +28,8 @@ import { RegisterComponent } from './login/register.component';
   imports: [
     BrowserModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,33 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { ChartsModule } from 'ng2-charts';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 
 @NgModule({
-    declarations: [
-        DashboardComponent,
-        ProgressComponent,
-        Graficas1Component,
-        PagesComponent
-    ],
-    imports: [
-        SharedModule,
-        PAGES_ROUTES
-    ],
-    exports: [
-        DashboardComponent,
-        ProgressComponent,
-        Graficas1Component,
-        PagesComponent
-    ],
-    providers: [],
-    bootstrap: []
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+    IncrementadorComponent,
+    GraficoDonaComponent
+  ],
+  imports: [SharedModule, PAGES_ROUTES, FormsModule, ChartsModule],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent
+  ],
+  providers: [],
+  bootstrap: []
 })
-
-export class PagesModule { }
+export class PagesModule {}
